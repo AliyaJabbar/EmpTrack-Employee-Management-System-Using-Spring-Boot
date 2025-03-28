37906287a30dc5a0167dec7a54d9a722952eb1e0
Aliya Jabbar <164192813+AliyaJabbar@users.noreply.github.com>
2025-03-28 14:27:20 +0530

    Update App.jsx

--- EmployeeFullStack/ems-frontend/src/App.jsx
+++ EmployeeFullStack/ems-frontend/src/App.jsx
@@ -1,13 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route,Navigate } from 'react-router-dom';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import UpdateEmployeeComponent from './components/UpdateEmployeeComponent';
+import DeleteEmployeeComponent from './components/DeleteEmployeeComponent';
 
 function App() {
     return (
         <Router>
             <Routes>
+            <Route path="/" element={<Navigate to="/employees" />} /> 
                 <Route path="/employees" element={<ListEmployeeComponent />} />
                 <Route path="/update-employee/:id" element={<UpdateEmployeeComponent />} />
+                <Route path="/delete-employee/:id" element={<DeleteEmployeeComponent />} /> {/* Add this route */}
+              
+                {/* <Route path="/delete-employee/:id" element={<DeleteEmployeeComponent />} /> */}
             </Routes>
         </Router>
     );
